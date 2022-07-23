834994b5e854ecc508f3dc75242711f5367a76fe
Miguel Angel de la Rosa Guerrero <migueeldrg@gmail.com>
2022-07-23 10:31:05 -0500

    add html2pdf library
    create invoice html template generator

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
+import type { NextApiRequest, NextApiResponse } from "next";
+import pdf, { CreateOptions } from "html-pdf";
+import invoiceHtmlTemplateGenerator from "../../lib/api/invoiceHtmlTemplateGenerator";
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const html = invoiceHtmlTemplateGenerator.generate({ items: [1, 2, 3, 4] });
+  const options: CreateOptions = { format: "Letter" };
+  pdf.create(html.toString(), options).toBuffer(function (err, buffer) {
+    res.setHeader("Content-Type", "application/pdf");
+    res.status(200).send(buffer);
+  });
 }
