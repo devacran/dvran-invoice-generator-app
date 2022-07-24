5e7f86483429035ded64cc89aba7d9636235643b
Miguel Angel de la Rosa Guerrero <migueeldrg@gmail.com>
2022-07-24 15:16:44 -0500

    move invoice gen page to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,12 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import { NextPage } from "next";
+import React from "react";
+import InvoiceGenerator from "../components/InvoiceGenerator";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <h1>
-        <Link href="/invoice-template-generator">
-          Start creating your Invoice
-        </Link>
-      </h1>
-    </div>
+    <main className="container">
+      <InvoiceGenerator />
+    </main>
   );
 };
 
